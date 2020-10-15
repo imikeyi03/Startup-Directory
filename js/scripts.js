f4fd6656e125fa5475d56fee0a6bb27dcc8bc97a
mikey mann <mikey.mannx3@gmail.com>
2020-10-15 07:32:41 -0700

    Pulled in data to create employee cards

--- js/scripts.js
+++ js/scripts.js
@@ -19,13 +19,30 @@ function fetchData(url) {
 
 // Get the random peoples data such as profileIMG,name,email,city,state 
 function getRandomPeople(data) {
-     console.log(`getRandomPeople ran! Check out the ${data}`);
-     return data;
+    const randomPeople = data.results.map(employee => {
+        return employee;
+    })
+   return randomPeople; 
 }
 
 
+
 function generateCards(data) {
-    console.log(`generateCards ran! Check out the ${data}`)
+    data.map(person => {
+        galleryDiv.insertAdjacentHTML('beforeend', `
+            <div class="card">
+                <div class="card-img-container">
+                    <img class="card-img" src="${person.picture.large}" alt="Profile Pic">
+                </div>
+                <div class="card-info-container">
+                        <h3 id="name" class="card-name cap">${person.name.first} ${person.name.last}</h3>
+                        <p class="card-text">${person.email}</p>
+                        <p class="card-text cap">${person.location.city}, ${person.location.state}</p>
+                    </div>
+                </div>
+            </div>
+        `)    
+    })
 }
 
 
