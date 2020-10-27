61632af4553e3d427b11f0e9da6941d199226d41
mikey mann <mikey.mannx3@gmail.com>
2020-10-27 08:11:24 -0700

    Got cycling buttons to fireer correctly

--- js/scripts.js
+++ js/scripts.js
@@ -32,7 +32,7 @@ function fetchData(url) {
                     }
                     
                     //Pass the matching employee, at that index, to handleCardClick() to display the modal 
-                    handleCardClick(employees[index]);
+                    handleCardClick(employees[index],index);
                 })
                
             }
@@ -42,33 +42,68 @@ function fetchData(url) {
 
 // When a card is clicked, display the modal
 
-function handleCardClick(employee) {
+function handleCardClick(employee, index) {
+    
     const html = `
     <div class="modal-container">
-    <div class="modal">
-        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-        <div class="modal-info-container">
-            <img class="modal-img" src="${employee.picture.large}" alt="${employee.name.first}\'s profile pic">
-            <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
-            <p class="modal-text">${employee.email}</p>
-            <p class="modal-text cap">${employee.location.city}</p>
-            <hr>
-            <p class="modal-text">${employee.cell.slice(0,5)} ${employee.cell.slice(6,14)}</p>
-            <p class="modal-text">${employee.location.street.number} ${employee.location.street.name}, ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
-            <p class="modal-text">Birthday: ${employee.dob.date.slice(5,7)}/${employee.dob.date.slice(8,10)}/${employee.dob.date.slice(0,4)}</p>
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src="${employee.picture.large}" alt="${employee.name.first}\'s profile pic">
+                <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
+                <p class="modal-text">${employee.email}</p>
+                <p class="modal-text cap">${employee.location.city}</p>
+                <hr>
+                <p class="modal-text">${employee.cell.slice(0,5)} ${employee.cell.slice(6,14)}</p>
+                <p class="modal-text">${employee.location.street.number} ${employee.location.street.name}, ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
+                <p class="modal-text">Birthday: ${employee.dob.date.slice(5,7)}/${employee.dob.date.slice(8,10)}/${employee.dob.date.slice(0,4)}</p>
+            </div>
         </div>
+            <div class="modal-btn-container">
+                <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                <button type="button" id="modal-next" class="modal-next btn">Next</button>
+            </div>
     </div>
     `;
-    body.insertAdjacentHTML('beforeEnd', html)
+    body.insertAdjacentHTML('beforeEnd', html);
+
 
     // Select the modal & on a click event on the close btn, remove() the modal from the DOM
-
     const modalContainer = document.querySelector('.modal-container');
     const modalBtn = document.querySelector('#modal-close-btn');
+    const modalPreviousBtn = document.querySelector('#modal-prev');
+    const modalNextBtn = document.querySelector('#modal-next');
+
+    if(index == 0) {
+        modalPreviousBtn.style.display = 'none';
+    }
+    if(index == 11) {
+        modalNextBtn.style.display = 'none';
+    }
+
+   
+
+    modalPreviousBtn.addEventListener('click', () => {
+         if(index >= 1 ) {
+            cycleCards(employees[index - 1], index - 1)
+        }
+    })
+
+    modalNextBtn.addEventListener('click', () => {
+        if(index < 11) {
+            cycleCards(employees[index + 1], index + 1)
+         }
+    })
     
     modalBtn.addEventListener('click', () => {
         modalContainer.remove();
     })
+    
+
+    function cycleCards(employee, index) {
+        document.querySelector('.modal-container').remove();
+        handleCardClick(employee, index)  
+    }
 }
 
 // Maps over each employee returned from Fetch and displays them in cards
@@ -92,6 +127,9 @@ function generateCards(data) {
     });
     return galleryDiv.children
 }
+
+
+
 
 
 
